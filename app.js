a1633e487389d383c45f291d989350146e077a2c
c <chadreed.intl@gmail.com>
2013-07-22 23:05:28 -0700

    sytling added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+
 var hbs = require('hbs');
 
 var blogEngine = require('./blog');
@@ -8,17 +9,19 @@ app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.bodyParser());
 
-app.get('/', function(request, response) {
-  response.render('index', {title:"My Blog", entries:blogEngine.getBlogEntries()});
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
 });
 
-app.get('/about', function(request, response) {
-	response.render('about', {title: "About Me"});
+app.get('/about', function(req, res) {
+	res.render('about', {title:"About Me"});
 });
 
-app.get('/article', function(request, response) {
-  var entry = blogEngine.getBlogEntry(request.params.id);
-  response.render('article', {title:entry.title, blog:entry});
+app.get('/article/:id', function(req, res) {
+	var entry = blogEngine.getBlogEntry(req.params.id);
+	res.render('article',{title:entry.title, blog:entry});
 });
 
 app.listen(3000);
--- blog.js
+++ blog.js
@@ -1,9 +1,9 @@
 var entries = [
 {"id":1, "title":"Hello Drax!", "body":"This is a site for you to explore. Click the other links posts for notes.", "published":"7/22/2013"},
-{"id":2, "title":"I heart you more than beer", "body":"You are more refreshing than an ice cold Fransiskaner on a hot day", "published":"7/22/2013"},
-{"id":3, "title":"Ich liebe deine lipen", "body":"I may not know German, but its cute when I try right?", "published":"7/22/2013"},
-{"id":4, "title":"Montage", "body":"We've done so much together, just think of all the amazing thing: Bermuda, Turkey, Aletch, trotti net?, semuc champey, hawaii, capilano bridge, empire state building, croatia...and on and on", "published":"7/22/2013"},
-{"id":5, "title":"We are pretty cool", "body":"I think we probably have the most fun as a couple. Look at what we can do with so little money, imagine when we both have money!", "published":"7/22/2013"},
+{"id":2, "title":"Coffee, Water, Beer", "body":"Repeat", "published":"7/22/2013"},
+{"id":3, "title":"Testing", "body":"Testing 1, Testing 1,2", "published":"7/22/2013"},
+{"id":4, "title":"Montage", "body":"French for fast forward", "published":"7/22/2013"},
+{"id":5, "title":"Bald Eagles", "body":"That is all", "published":"7/22/2013"},
 {"id":6, "title":"Thanks for looking", "body":"Nothing else to see here", "published":"7/22/2013"}];
 
 exports.getBlogEntries = function() {
